ee1df576f4e48e2322ab7521a42749d1ebfc9509
Dom Sinclair <dom@viewtolearn.co.uk>
2023-09-01 10:48:37 +0100

    Add project files.

---
+++ VtlSoftware.Aspects.Common.NetStandard21/Guards/LogRecursionGuard.cs
@@ -0,0 +1,107 @@
+﻿namespace VtlSoftware.Aspects.Common.NetStandard21
+{
+    /// <summary>
+    /// A log recursion guard.
+    /// </summary>
+    ///
+    /// <remarks></remarks>
+    ///
+    /// ### <remarks>.</remarks>
+
+    public static class LogRecursionGuard
+    {
+        #region Fields
+        /// <summary>
+        /// True if is logging, false if not.
+        /// </summary>
+        [ThreadStatic]
+        public static bool isLogging;
+
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Gets the begin.
+        /// </summary>
+        ///
+        /// <remarks></remarks>
+        ///
+        /// <returns>A DisposeCookie.</returns>
+        ///
+        /// ### <remarks>.</remarks>
+
+        public static DisposeCookie Begin()
+        {
+            if(isLogging)
+            {
+                return new DisposeCookie(false);
+            } else
+            {
+                isLogging = true;
+                return new DisposeCookie(true);
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// A dispose cookie.
+        /// </summary>
+        ///
+        /// <remarks></remarks>
+        ///
+        /// <seealso cref="T:IDisposable"/>
+        ///
+        /// ### <remarks>.</remarks>
+
+        public class DisposeCookie : IDisposable
+        {
+            #region Constructors
+            /// <summary>
+            /// Constructor.
+            /// </summary>
+            ///
+            /// <remarks></remarks>
+            ///
+            /// <param name="canLog">True if we can log, false if not.</param>
+            ///
+            /// ### <remarks>.</remarks>
+
+            public DisposeCookie(bool canLog) { this.CanLog = canLog; }
+
+            #endregion
+
+            #region Public Methods
+            /// <summary>
+            /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+            /// </summary>
+            ///
+            /// <remarks></remarks>
+            ///
+            /// <seealso cref="M:IDisposable.Dispose()"/>
+            ///
+            /// ### <remarks>.</remarks>
+
+            public void Dispose()
+            {
+                if(this.CanLog)
+                {
+                    isLogging = false;
+                }
+            }
+
+            #endregion
+
+            #region Public Properties
+            /// <summary>
+            /// Gets a value indicating whether we can log.
+            /// </summary>
+            ///
+            /// <value>True if we can log, false if not.</value>
+
+            public bool CanLog { get; }
+
+            #endregion
+        }
+    }
+}
---
+++ VtlSoftware.Aspects.Common.NetStandard21/ReadMe.md
@@ -0,0 +1,5 @@
+# Vtl Software Aspects Common (Net Standard 2.1)
+
+This package contains some basic functionality required by Vtl Software Aspect packages.
+
+This particular version targets .Net Standard 2.1
